dcba120cc7a9e3837f92f1f110673c873ef3be34
fed8055 <fed8055@yandex.ru>
2020-05-15 10:03:34 +0700

    first

---
+++ apiEngine.php
@@ -0,0 +1,13 @@
+<?php
+    class apiEngine{
+        private $method;
+        private $value;
+
+        public function __construct(){
+
+        }
+
+        function findClass(){
+
+        }
+    }
